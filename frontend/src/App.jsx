2fafec23df7b582cc2b3b3a6cbe0ceec9f4ef5c2
omp19 <aneeshkushwaha2004@gmail.com>
2026-02-21 19:10:04 +0530

    feat: add Login and Signup pages with glassmorphic Card UI and theme-based styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import StudentDashboard from './pages/learning/Dashboard';
+import Login from './pages/auth/Login';
+import Signup from './pages/auth/Signup';
 import './index.css';
 
 /**
@@ -14,6 +16,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<StudentDashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
